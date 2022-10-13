a7bf43fa1095760c5f4422f3e05998f86a54f0fc
Sarah Hampton <stoluba@gmail.com>
2022-10-13 19:41:45 -0400

    Complete createMenuItem

--- src/meal.js
+++ src/meal.js
@@ -3,10 +3,22 @@ function nameMenuItem(menuItemName) {
   return `Delicious ${menuItemName}`
 };
 
+function createMenuItem() {
+return menuItem = {
+  name: "Delicious French Toast",
+  price: 10.99,
+  type: "breakfast"
+};
+
+};
+
+
+
+
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
