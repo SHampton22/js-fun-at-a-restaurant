5f80d3a816b54525f9be6fa7989e7b362d449de2
Sarah Hampton <stoluba@gmail.com>
2022-10-12 22:00:00 -0400

    Complete warm-up

--- src/warm-up.js
+++ src/warm-up.js
@@ -3,7 +3,7 @@
 // become comfortable with these fundamental concepts. Do not move on to
 // iteration 1 until you are confident that you've completed all of iteration 0.
 
-const { should } = require("chai");
+//const { should } = require("chai");
 
 //------------------------- Variables -------------------------//
 
@@ -50,41 +50,56 @@ if (inStock < 20) {
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
 // the statement 'I'm going to eat some [food]!', otherwise, log the statement 
 // 'We need to get some more [food].'
-
-
+var stockCount = inStock;
+if (stockCount >= 1) {
+    console.log(`I'm going to eat some ${food}!`)
+} else {
+    console.log(`We need to get some more ${food}.`)
+};
+// should return "I'm going to eat some Ice cream" since inStock = 19.
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
-
+if (isFavorite === true) {
+    console.log(`My favorit food is ${food}!`);
+} else {
+    console.log(`I recently ate ${food}.`)
+};
 
 //------------------------- Arrays -------------------------//
 
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
-
+var vegetables = ['carrots', 'peas', 'cucumbers', 'broccoli', 'green beans'];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+vegetables[2];
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
-
+var fruits = ['grapes', 'blueberries', 'strawberries', 'pineapples', 'watermelon', 'apples', 'oranges'];
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+fruits[4];
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable called "groceryStore" and assign to it an object literal with two
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
-
-
+var groceryStore = {
+        name: 'King Soopers',
+        isOpen: true
+};
+//weeklySpecials:
+//departments:
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+groceryStore.location = 'Denver, Colorado';
+//console.log(groceryStore);
